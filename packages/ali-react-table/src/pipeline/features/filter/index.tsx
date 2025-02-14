fe2e326d0c1d4d13b6fbda109143988ce640ab4c
SunLxy <1011771396@qq.com>
2025-02-14 11:34:31 +0800

    feat:调整告警

--- packages/ali-react-table/src/pipeline/features/filter/index.tsx
+++ packages/ali-react-table/src/pipeline/features/filter/index.tsx
@@ -53,7 +53,7 @@ const FilterHeaderCell = styled.div`
   align-items: center;
 `
 
-function FilterIcon(props: HTMLAttributes<HTMLOrSVGElement>) {
+const FilterIcon = React.forwardRef<SVGSVGElement, HTMLAttributes<HTMLOrSVGElement>>((props, ref) => {
   return <Svg
     viewBox="64 64 896 896"
     focusable="false"
@@ -63,12 +63,13 @@ function FilterIcon(props: HTMLAttributes<HTMLOrSVGElement>) {
     fill="currentColor"
     aria-hidden="true"
     {...props}
+    ref={ref}
   >
     <path
       d="M349 838c0 17.7 14.2 32 31.8 32h262.4c17.6 0 31.8-14.3 31.8-32V642H349v196zm531.1-684H143.9c-24.5 0-39.8 26.7-27.5 48l221.3 376h348.8l221.3-376c12.1-21.3-3.2-48-27.7-48z" />
   </Svg>
 }
-
+)
 
 export interface FilterHeaderCellProps {
   /** 在添加排序相关的内容之前 表头原有的渲染内容 */
